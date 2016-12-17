16e43515c697552b1195bbbe649f073399f4fa91
Prashanth D V S <prashdvs@thoughtworks.com>
2016-12-17 16:33:03 +0530

    highlighting line number for each frame

--- js/code_viz.js
+++ js/code_viz.js
@@ -4,7 +4,7 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/python");
 
-function visualized() {
+function visualize() {
 
     var send = {
         "user_script": editor.getValue(),
@@ -13,7 +13,7 @@ function visualized() {
         "diffs_json": "{}",
         "raw_input_json": "",
         "options_json": '{"cumulative_mode":false,"heap_primitives":false,"show_only_outputs":false,"origin":"opt-frontend.js"}'
-    }
+    };
 
     $.post("/cgi-bin/web_exec_py2.py", send, function (data) {
         var codeResponse = JSON.parse(data);
@@ -21,45 +21,38 @@ function visualized() {
     });
 
 
-    d3.select("#visualize").selectAll("svg").remove();
-    var svg = d3.select("#visualize").append("svg").attr("width", 1000).attr("height", 100),
-        global = svg.append("g")
-
     function visualizeTrace(trace) {
-        var globalData = trace.map(function (element) {
-            return element["globals"]
-        });
-        globalFrameRender(globalData);
+        d3.select("#visualize").selectAll("svg").remove();
+        var svg = d3.select("#visualize").append("svg").style("width", "46em").style("height", "43em").style("background", "#eee");
+        var index = 0;
+        var renderInterval = setInterval(function () {
+            if (index == trace.length - 1) {
+                clearInterval(renderInterval);
+            }
+            renderCurrentStep(trace[index]);
+            index += 1;
+        }, 2000)
     }
 
-    function globalFrameRender(data) {
-        var element = 0
-        var intervalD3 = setInterval(function () {
-            console.log(element == data.length, data.length, element);
-            if (element == data.length - 1) {
-                clearInterval(intervalD3);
-            }
-            var globalObject = data[element];
-            var text = global.selectAll("text")
-                             .data([globalObject], function (d) {
-                                 return JSON.stringify(d["a"]);
-                             });
+    function renderCurrentStep(currentStep) {
+        // Global
 
-            text.exit()
-                .attr("class", "exit")
-                .attr("y", 60)
-                .remove();
 
-            text.enter()
-                .append("text")
-                .attr("y", 60)
-                .attr("x", function(d, i) { return i * 100; })
-                .text(function(d) {
-                    return JSON.stringify(d);
-                });
+        renderGlobalFrame(currentStep["global"]);
+        // Heap
 
-            element = element + 1;
-        }, 1000);
+        highlightLine(currentStep["line"]);
+    }
+
+    function renderGlobalFrame(globalFrame) {
+
+    }
+
+    function highlightLine(lineNumber) {
+        $(".ace_line").removeClass("mark-line");
+        $(".ace_gutter-cell").removeClass("mark-line");
+        $(".ace_line:nth-child(" + lineNumber + ")").addClass("mark-line");
+        $(".ace_gutter-cell:nth-child(" + lineNumber + ")").addClass("mark-line");
     }
 
 
