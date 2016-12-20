1c305b09ac7b4c74447434159f718131e5d6212f
Devashish Sood <devasood@gmail.com>
2016-12-20 16:14:53 +0530

    Devashish: Set up gulp to resolve dependencies

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+
+gulp.task('editor', function() {
+  console.log("Cloning Ace Editor in background...");
+  git.clone('https://github.com/ajaxorg/ace.git',
+            {args:'./app/ace-builds'}, function(err) {
+    if (err) throw err;
+  });
+});
+
