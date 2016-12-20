0f634b4bb6066e1b5143d4a13361019221a1e48f
Devashish Sood <devasood@gmail.com>
2016-12-20 16:30:11 +0530

    Devashish: Fixed Ace editor link

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var git = require('gulp-git');
 
 gulp.task('editor', function() {
   console.log("Cloning Ace Editor in background...");
-  git.clone('"Devashish: Set up gulp to resolve dependencies"',
+  git.clone('https://github.com/ajaxorg/ace-builds.git',
             {args:'./app/ace-builds'}, function(err) {
     if (err) throw err;
   });
