c4d164196f1d95b65ee6ede7ca795c98759f83ed
Prashanth D V S <prashdvs@thoughtworks.com>
2016-12-20 14:14:02 +0530

    Added d3 groups for global frame and variables

--- js/code_viz.js
+++ js/code_viz.js
@@ -20,10 +20,14 @@ function visualize() {
         visualizeTrace(codeResponse["trace"]);
     });
 
+    d3.select("#visualize").selectAll("svg").remove();
+    var svg = d3.select("#visualize").append("svg").style("width", "46em").style("height", "43em").style("background", "#eee");
+    var all_frames = svg.append("g").attr("id", "main_group");
+    var global_frame = all_frames.append("g").attr("id", "global_frame_group");
+    var elements_in_global_frame = global_frame.append("g").attr("class", "elements_group");
 
+    global_frame.append("circle").attr("cx", 300).attr("cy", 300).attr("r", 250);
     function visualizeTrace(trace) {
-        d3.select("#visualize").selectAll("svg").remove();
-        var svg = d3.select("#visualize").append("svg").style("width", "46em").style("height", "43em").style("background", "#eee");
         var index = 0;
         var renderInterval = setInterval(function () {
             if (index == trace.length - 1) {
@@ -35,16 +39,48 @@ function visualize() {
     }
 
     function renderCurrentStep(currentStep) {
-        // Global
 
+        renderGlobalFrame(currentStep["globals"]);
 
-        renderGlobalFrame(currentStep["global"]);
         // Heap
-
         highlightLine(currentStep["line"]);
     }
 
     function renderGlobalFrame(globalFrame) {
+        var previous_cx = 0;
+
+        elements_in_global_frame.selectAll("circle")
+            .data(Object.keys(globalFrame), function (d) {
+                return d;
+            })
+            .enter()
+            .append("circle")
+            .attr("r", function (d) {
+                return d.length * 10;
+            })
+            .attr("cx", function (d, i) {
+                previous_cx+=1;
+                return 150 * (2 *(i + 1)) + 2 * d.length * 10;
+            })
+            .attr("cy", 50)
+            .attr("class", "variable_circle");
+
+        console.log(Object.keys(globalFrame));
+
+        elements_in_global_frame.selectAll("text")
+            .data(Object.keys(globalFrame), function(d) {return d;})
+            .enter()
+            .append("text")
+            .attr("class", "variable_text")
+            .attr("x", function (d, i) {
+                return 150 * (i + 1) + d.length * 10 + 150;
+            })
+            .attr("y", 50)
+            .attr("fill", "red")
+            .text(function (d) {
+                console.log(d);
+                return d;
+            })
 
     }
 
