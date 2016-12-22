ea89b76ce68cf9e84a988cf0fc48bb6f9e52aae1
Prashanth D V S <prashdvs@thoughtworks.com>
2016-12-22 15:58:36 +0530

    Displaying elements of the heap

--- js/code_viz.js
+++ js/code_viz.js
@@ -23,12 +23,12 @@ function visualize() {
 
     d3.select("#visualize").selectAll("svg").remove();
     var svg = d3.select("#visualize")
-                .append("svg")
-                .style("width", "46em")
-                .style("height", "43em")
-                .style("background", "#eee");
-    svg.append("g").attr("id", "global_vars").attr("transform", "translate(20, 20)");
+                .append("svg");
 
+    // TODO: Change height of svg based on number of elements
+
+    svg.append("g").attr("id", "global_vars").attr("transform", "translate(20, 20)");
+    svg.append("g").attr("id", "heap").attr("transform", "translate(300, 20)");
 
     function visualizeTrace(trace) {
         var index = 0;
@@ -38,19 +38,64 @@ function visualize() {
             }
             renderCurrentStep(trace[index]);
             index += 1;
-        }, 2000);
+        }, 500);
     }
 
     function renderCurrentStep(currentStep) {
         renderGlobalFrame(currentStep.globals, currentStep.ordered_globals);
 
-        // Heap
+        renderHeap(currentStep.heap);
 
         highlightLine(currentStep.line);
     }
 
+    function renderHeap(heap) {
+        function range(count) {
+            return Array.apply(0, Array(count))
+                .map(function (element, index) {
+                    return index + 1;
+                });
+        }
+        function stringify(type, arr) {
+            console.log(type, arr);
+            if (type == "LIST") {
+                return "LIST => [" + arr.join(", ") + "]";
+            } else if (type == "SET") {
+                return "SET => {" + arr.join(", ") + "}";
+            } else if (type == "TUPLE") {
+                return "TUPLE => (" + arr.join(", ") + ")";
+            } else if (type == "DICT") {
+                return "DICT => {" + arr.map(function (data) {
+                        return data[0] + ":" + data[1];
+                    }).join(", ") + "}"
+            }
+        }
+        var text = d3.select("#heap")
+            .selectAll("text")
+            .data(range(Object.keys(heap).length));
+        var rects = d3.select("#heap")
+            .selectAll("rect")
+            .data(range(Object.keys(heap).length));
+
+        rects.enter()
+            .append("rect")
+            .attr("class", "heap_rect")
+            .attr("x", "0")
+            .attr("y", function(d,i) { return i * 40; })
+            .attr("width", function (d) { return 30 + 10 * stringify(heap[d][0], heap[d].slice(1)).length; })
+            .attr("height", "30");
+
+        text.enter()
+            .append("text")
+            .attr("class", "heap_text")
+            .attr("dx", "17")
+            .attr("dy", function(d,i) { return 19 + i * 40; })
+            .text(function (d) {
+                return stringify(heap[d][0], heap[d].slice(1));
+            });
+    }
+
     function renderGlobalFrame(globalFrame, globalKeys) {
-        console.log(globalFrame);
         var text = d3.select("#global_vars")
                       .selectAll("text")
                       .data(globalKeys);
@@ -62,14 +107,28 @@ function visualize() {
              .attr("class", "global_var_rect")
              .attr("x", "0")
              .attr("y", function(d,i) { return i * 40; })
-             .attr("width", function(d) { return 30 + 10 * (d+" = "+globalFrame[d]).length;})
+             .attr("width", function(d) {
+                 if (typeof globalFrame[d] == "object")
+                     return 30 + 10 * d.length;
+                 return 30 + 10 * (d+" = "+globalFrame[d]).length;
+             })
              .attr("height", "30");
         text.enter()
             .append("text")
             .attr("class", "global_var_text")
             .attr("dx", "17")
             .attr("dy", function(d,i) { return 19 + i * 40; })
-            .text(function(d) { return d + " = " + globalFrame[d]; });
+            .text(function(d) {
+                if (typeof globalFrame[d] == "object")
+                    return d;
+                return d + " = " + globalFrame[d];
+            });
+
+        text.text(function(d) {
+            if (typeof globalFrame[d] == "object")
+                return d;
+            return d + " = " + globalFrame[d];
+        });
     }
 
     function highlightLine(lineNumber) {
